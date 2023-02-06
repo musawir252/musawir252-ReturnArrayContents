c2a07bc73eb3ce9f79b7955baffadfe1da127cc3
musawir252 <musawir252@gmail.com>
2023-02-06 01:00:18 +0000

    Completed Return Array Contents

--- src/main/java/ReturnArrayContents.java
+++ src/main/java/ReturnArrayContents.java
@@ -28,6 +28,15 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String str = "";
+        
+      for(int i = 0; i < nums.length; i++){
+           //do something to str here
+           str = str + String.valueOf(nums[i] + " ");
+          // System.out.print(str);
+          
+      }
+      return str;
+      
     }
 }
